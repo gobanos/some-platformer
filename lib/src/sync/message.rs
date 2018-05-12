cb45e1228cfae91200aa67ce6c29c272d96c3de5
Grégory Obanos <gregory.obanos@gmail.com>
2018-05-12 10:31:03 +0200

    correct a typo

--- lib/src/sync/message.rs
+++ lib/src/sync/message.rs
@@ -2,7 +2,7 @@ use std::time::SystemTime;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Client {
-    Test,             // An empty message, to test protocoles
+    Test,             // An empty message, to test protocols
     Ping(SystemTime), // Current time, to synchronize client and server
 }
 
